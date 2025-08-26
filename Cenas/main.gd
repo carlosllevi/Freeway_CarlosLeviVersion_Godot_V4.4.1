6017225484ddda6184d886628db54e14e27e9c9d
carlosllevi <carlos2levi@gmail.com>
2025-08-26 16:37:32 -0300

    2° Player Adicionado

--- Cenas/main.gd
+++ Cenas/main.gd
@@ -4,6 +4,7 @@ var cena_carros = preload("res://Cenas/carros.tscn")
 var pistas_rapidas_y = [104, 272, 488]
 var pistas_lentas_y = [160, 216, 324, 384, 438, 544, 600]
 var score = 0
+var tempo_restante: int=60
 
 func _ready() -> void: #A gente coloca tudo que vai acontecer ao abri a cena principal
 	$HUD/Placar.text = str(score) # faz com que o placar atualize de valor
@@ -11,6 +12,8 @@ func _ready() -> void: #A gente coloca tudo que vai acontecer ao abri a cena pri
 	$HUD/Button.hide()
 	$AudioTema.play()
 	randomize()
+	$Player.pontua.connect(Callable(self, "_on_player_pontua").bind(1))
+	$Player2.pontua.connect(Callable(self, "_on_player_pontua").bind(2))
 
 func _on_timer_carros_rapidos_timeout() -> void:
 	var carro = cena_carros.instantiate()
@@ -28,12 +31,15 @@ func _on_timer_carros_lentos_timeout() -> void:
 	carro.set_linear_velocity(Vector2(randf_range(300.0, 320.0), 0))
 	carro.set_linear_damp(0.0) 
 
-func _on_player_pontua() -> void:
+func _on_player_pontua(player_num: int) -> void:
 	if score <= 10:
 		score +=1
 		$HUD/Placar.text = str(score)
 		$AudioPonto.play()
-		$Player.position = $Player.posicao_inicial
+		if player_num == 1:
+			$Player1.position = $Player1.posicao_inicial_player1
+		else:
+			$Player2.position = $Player2.posicao_inicial_player2
 	if score == 10:
 		$HUD/Mensagem.text = "Parabén Você Venceu!!!"
 		$HUD/Button.show()
@@ -41,11 +47,15 @@ func _on_player_pontua() -> void:
 		$TimerCarrosLentos.stop()
 		$AudioVitoria.play()
 		$Player. speed = 0
-		
+		$Player2. speed = 0
 
 func _on_hud_reinicia() -> void:
 	get_tree().reload_current_scene()
 
 
 func _on_timer_timeout() -> void: # timer do game over
-	$LabelGameOver.text = "Game-Over!!!"
+	tempo_restante-=1
+	$LabelTimer.text = str(tempo_restante)
+	if tempo_restante <= 0:
+		$Timer.stop()
+		$LabelGameOver.text = "Game-Over!!!"
--- Cenas/player.gd
+++ Cenas/player.gd
@@ -1,26 +1,40 @@
 extends Area2D
 signal pontua
 # variáveis
-@export var speed: float = 200.0 #export serv para fazer aparecer no painel inspetor
+@export var speed: float = 100.0 #export serve para fazer aparecer no painel inspetor
+@export var player_id: int = 1
 var screen_size: Vector2
-var posicao_inicial: Vector2 = Vector2(640, 690) 
+var posicao_inicial_player1: Vector2 = Vector2(450, 690) 
+var posicao_inicial_player2: Vector2 = Vector2(750, 690) 
 
 #funções
 func _ready() -> void: 
 	screen_size = get_viewport_rect().size # armazena o valor da tela
-	position = posicao_inicial #armazenar o valor da posição
+	if player_id == 1:
+		position =posicao_inicial_player1
+	else:
+		position = posicao_inicial_player2 #armazenar o valor da posição
 
 func _process(delta: float) -> void:
 	var velocity = Vector2.ZERO
-	
-	if Input.is_action_pressed("ui_up"): # Quando for pressionado a galinah vai andar no sentido Y
-		velocity.y -=1
-	if Input.is_action_pressed("ui_down"):
-		velocity.y +=1
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -=1
-	if Input.is_action_pressed("ui_right"):
-		velocity.x +=1
+	if player_id == 2:
+		if Input.is_action_pressed("ui_up"): # Quando for pressionado a galinah vai andar no sentido Y
+			velocity.y -=1
+		if Input.is_action_pressed("ui_down"):
+			velocity.y +=1
+		if Input.is_action_pressed("ui_left"):
+			velocity.x -=1
+		if Input.is_action_pressed("ui_right"):
+			velocity.x +=1
+	if player_id == 1:
+		if Input.is_action_pressed("p1_up"): # Quando for pressionado a galinah vai andar no sentido Y
+			velocity.y -=1
+		if Input.is_action_pressed("p1_down"):
+			velocity.y +=1
+		if Input.is_action_pressed("p1_left"):
+			velocity.x -=1
+		if Input.is_action_pressed("p1_right"):
+			velocity.x +=1
 	
 	if velocity != Vector2.ZERO:
 		velocity = velocity.normalized() * speed
@@ -40,4 +54,7 @@ func _on_body_entered(body: Node2D) -> void:
 		emit_signal("pontua")
 	else: 
 		$Audio.play()
-		position = posicao_inicial
+		if player_id == 1:
+			position =posicao_inicial_player1
+		else:
+			position = posicao_inicial_player2
